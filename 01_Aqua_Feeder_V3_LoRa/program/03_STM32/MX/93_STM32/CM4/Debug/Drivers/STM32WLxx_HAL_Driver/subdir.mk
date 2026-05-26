dcb08abc241cf4f7b227ad5d91ed70dd04c123d3
codewithsam404 <codewithsam404@gmail.com>
2026-05-26 11:12:49 +0530

    Tested LED Blue and green, it is ioc

---
+++ 01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/CM4/Debug/Drivers/STM32WLxx_HAL_Driver/subdir.mk
@@ -0,0 +1,92 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (14.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_cortex.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_dma.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_dma_ex.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_exti.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_flash.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_flash_ex.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_gpio.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_pwr.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_pwr_ex.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_rcc.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_rcc_ex.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_uart.c \
+/home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_uart_ex.c 
+
+OBJS += \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.o \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.o 
+
+C_DEPS += \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.d \
+./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_cortex.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_dma.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_dma_ex.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_exti.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_flash.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_flash_ex.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_gpio.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_pwr.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_pwr_ex.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_rcc.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_rcc_ex.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_uart.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.o: /home/samuel/git/all/01_Aqua_Feeder_V3_LoRa/program/03_STM32/MX/93_STM32/Drivers/STM32WLxx_HAL_Driver/Src/stm32wlxx_hal_uart_ex.c Drivers/STM32WLxx_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-STM32WLxx_HAL_Driver
+
+clean-Drivers-2f-STM32WLxx_HAL_Driver:
+	-$(RM) ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_cortex.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_dma_ex.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_exti.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_flash_ex.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_gpio.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_pwr_ex.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_rcc_ex.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart.su ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.cyclo ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.d ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.o ./Drivers/STM32WLxx_HAL_Driver/stm32wlxx_hal_uart_ex.su
+
+.PHONY: clean-Drivers-2f-STM32WLxx_HAL_Driver
+
